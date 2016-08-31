eb206f50d701e6b554e066690c082700041d3c1b
luolinhan <luolinhan666@sina.com>
2016-08-31 11:22:26 +0800

    Create endlessloop.py

---
+++ endlessloop.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*-coding:UTF-8 -*-
+var=1
+while var == 1:#该条件永远为true，循环一直进行下去
+        num = raw_input("Enter a number :")
+        print "Your entered:",num
+print "goodbye"
