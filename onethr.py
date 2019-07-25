157875fda228051b5f1858b1cc40ce882e89275f
luolinhan <luolinhan666@sina.com>
2019-07-25 18:02:16 +0800

    python-

--- onethr.py
+++ onethr.py
@@ -3,20 +3,20 @@
 from time import sleep,ctime
 
 def loop0():
-        print 'start loop 0 at:',ctime()
+        print ('start loop 0 at:'),ctime()
         sleep(4)
-        print 'loop 0 done at:',ctime()
+        print ('loop 0 done at:'),ctime()
 
 def loop1():
-        print 'start loop 1 at:',ctime()
+        print ('start loop 1 at:'),ctime()
         sleep(2)
-        print 'loop 1 done at:',ctime()
+        print ('loop 1 done at:'),ctime()
 
 def main():
-        print 'starting at:',ctime()
+        print ('starting at:'),ctime()
         loop0()
         loop1()
-        print 'all DONE at:',ctime()
+        print ('all DONE at:'),ctime()
 
 if __name__ == '__main__':
         main()
